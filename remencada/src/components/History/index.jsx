57afca5e6ed8c997f7d0fa5dba88c018a92c120a
ulldecorb <ausias.bertran@gmail.com>
2022-02-23 17:15:30 +0100

    Refactor style on history component

--- remencada/src/components/History/index.jsx
+++ remencada/src/components/History/index.jsx
@@ -1,47 +1,18 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import './History.css';
 
 export const History = function History({ data }) {
+  // const { title, description } = data[0];
+  // console.log(typeof (description));
   return (
-    <section className="historia">
-      {data.map((article) => (
-        <article key={article.title}>
-          <h2>{article.title}</h2>
-          <p>{article.description}</p>
+    <section className="history">
+      {data.map((enter) => (
+        <article key={enter.title}>
+          <h2>{enter.title}</h2>
+          <p>{enter.description}</p>
         </article>
       ))}
-      {/* <h2 className="historia__title">HISTÒRIA DE LES GUERRES REMENCES</h2>
-      <p>
-        La situació de la pagesia catalana s&apos;havia agreujat al segle XV per
-        un enduriment de les lleis feudals que volia compensar amb nous impostos
-        la pèrdua de població causada per les epidèmies de pesta negra.
-        <br />
-        Les sis lleis més odiades per injustes eren anomenades Els Sis Mals
-        Usos i centraven les reivindicacions dels  serfs de remença, contra
-        la negativa tancada dels senyors a abolir-les.
-        <br />
-        El 1462 va esclatar la insurrecció armada dels pagesos al nord del
-        Principat, dirigida per Francesc Verntallat, un estrateg excepcional.
-        La revolta remença es va creuar amb la guerra del rei Joan II contra
-        la noblesa catalana.
-        <br />
-        La pau senyorial, el 1472, va deixar les reivindicacions remences sense
-        resoldre.
-        <br />
-        El juliol de 1484, Pere-Joan Sala va iniciar a Mieres la segona insurrecció,
-        que es va estendre a moltes més comarques. El 4 de gener de 1485, les tropes
-        remences de Bartomeu Sala van derrotar a Montornès del Vallès l&apos;exèrcit de
-        la Diputació comandat pel veguer Pere-Anton de Rocacrespa.
-        <br />
-        La victòria pagesa va induir el rei Ferran II el Catòlic a enviar un militar
-        de prestigi, Iñigo López de Mendoza a fer de mitjancer entre les parts.
-        Finalment, el 22 d&apos;abril de 1486 se signava la Sentència Arbitral de Guadalupe
-        que posava fi al feudalisme a  Catalunya, primer país europeu a aconseguir-ho.
-        <br />
-        Les conseqüències van ser importants: el nivell de vida de la pagesia catalana
-        va augmentar considerablement, la població del Principat es va duplicar i es
-        van establir les bases per a la industrialització catalana dels segles XVII i XVIII.
-      </p> */}
     </section>
   );
 };
