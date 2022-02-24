e0df158086ab72e0f70bb297cc1f71964e39e6c8
ulldecorb <ausias.bertran@gmail.com>
2022-02-24 22:55:20 +0100

    Add videos on gallery

--- remencada/src/components/gallery/index.jsx
+++ remencada/src/components/gallery/index.jsx
@@ -9,6 +9,12 @@ export const Gallery = function Gallery() {
         <img src="http://www.laremencada.cat/4/images/1000_0_5193300_170891.jpg" alt="les remençes" className="slider__image" />
         <img src="http://www.laremencada.cat/4/images/1000_0_5193301_170891.jpg" alt="soldats de la diputació" className="slider__image" />
       </div>
+      <div className="gallery__video-box">
+        <iframe className="video-box__video" width="560" height="315" src="https://www.youtube.com/embed/GQZHFL8cSn4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe className="video-box__video" width="560" height="315" src="https://www.youtube.com/embed/ZTnPh1UMg1I" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe className="video-box__video" width="560" height="315" src="https://www.youtube.com/embed/qkHJOy6rh_4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe className="video-box__video" width="560" height="315" src="https://www.youtube.com/embed/Qi-HT64mMWg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      </div>
     </section>
   );
 };
