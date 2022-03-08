8f1eb916891f1f0fbc44021a8276b700ef724f0c
ulldecorb <ausias.bertran@gmail.com>
2022-03-08 11:06:17 +0100

    Complete parallax content

--- client/src/components/AboutUs/index.jsx
+++ client/src/components/AboutUs/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import { PropTypes } from 'prop-types';
 import './aboutUs.css';
 
 export const AboutUs = function AboutUs() {
@@ -26,42 +25,71 @@ export const AboutUs = function AboutUs() {
           <p>{article.p4}</p>
         </article>
       ))}
-      <h2>Remençada</h2>
+      <h2>Actes de l&#39;Obra</h2>
       <div className="parallax parallax__a" />
       <p className="parallax__txt">
         Acte 1er: L&#39;INCENDI DELS PALLERS
         <br />
         Un foc fortuït crema els pallers del senyor feudal.
+        <br />
         Els remences estan obligats per llei a sufragar-ne els danys.
+        <br />
       </p>
       <div className="parallax parallax__b" />
       <p className="parallax__txt">
         Acte 2on: BARTOMEU HA DE FUGIR
         <br />
         L&#39;amo reclama la filla del remença Bartomeu Sala per a minyona del castell.
+        <br />
         El pare es nega a cedir-la.
+        <br />
         S&#39;anuncien les represàlies del senyor feudal i Bartomeu ha d&#39;escapar.
+        <br />
       </p>
       <div className="parallax parallax__c" />
       <p className="parallax__txt">
-        Acte 3er:  LA REVOLTA ARRIBA AL VALLÈS
+        Acte 3er: LA REVOLTA ARRIBA AL VALLÈS
         <br />
         El mercat setmanal es veu trastornat per la proximitat de les tropes rebels.
+        <br />
         Cada pagès s&#39;ha de definir a favor o en contra de la revolta.
+        <br />
         Els joves es volen unir a la rebel·lió.
       </p>
       <div className="parallax parallax__d" />
+      <p className="parallax__txt">
+        Acte 4rt: LA DIPUTACIÓ ATACA MONTORNÈS
+        <br />
+        Bartomeu Sala ha tornat al poble com comandant de la insurrecció pagesa,
+        <br />
+        però la Diputació ha enviat un important exèrcit  feudal a liquidar la
+        revolta  dels remences.
+      </p>
+      <div className="parallax parallax__e" />
+      <p className="parallax__txt">
+        Acte 5è: NEGOCIACIONS DE PAU
+        <br />
+        El rei Ferran II ha enviat un mitjancer que negocia la pau amb els remences
+        i amb els senyors feudals.
+        <br />
+        Tot el país viu en un difícil compàs d&#39;espera.
+      </p>
+      <div className="parallax parallax__f" />
+      <p className="parallax__txt">
+        Acte 6è: EL FUTUR HA COMENÇAT
+        <br />
+        Amb la victòria de la revolució remença,
+        <br />
+        la vida dels pagesos catalans ha millorat considerablement.
+        <br />
+        Hi ha pau al país, i el rei dicta l&#39;amnistia per a tots els revoltats.
+        <br />
+        Bartomeu Sala pot tornar a Montornès amb la família.
+      </p>
+      <div className="parallax parallax__g" />
 
     </section>
   );
 };
-
-// AboutUs.propTypes = {
-//   data: PropTypes.arrayOf(
-//     PropTypes.object
-//   ).isRequired
-//   // title: PropTypes.string.isRequired,
-//   // description: PropTypes.string.isRequired
-// };
 
 export default AboutUs;
